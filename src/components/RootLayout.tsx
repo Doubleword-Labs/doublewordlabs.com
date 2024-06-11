48d23f27d2227370c0088b19ecfa442ead952540
Simon Hartcher <simon@simonhartcher.com>
2024-06-11 16:46:52 +1000

    feat: add doublewordlabs logos

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -105,7 +105,7 @@ export function RootLayout({ children }: { children: React.ReactNode }) {
       >
         <motion.div
           layout
-          className="relative isolate flex w-full flex-col pt-9"
+          className="relative isolate flex w-full flex-col pt-20"
         >
           <GridPattern
             className="absolute inset-x-0 -top-14 -z-10 h-[1000px] w-full fill-neutral-50 stroke-neutral-950/5 [mask-image:linear-gradient(to_bottom_left,white_40%,transparent_50%)]"

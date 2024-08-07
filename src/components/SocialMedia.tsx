60161ac1fdc533f1728b5da5c5b826a43b84579e
add-soul <167261983+add-soul@users.noreply.github.com>
2024-08-07 14:25:43 +1000

    Update SocialMedia.tsx
    updated FB and Instagram links

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -49,8 +49,8 @@ function DribbbleIcon(props: React.ComponentPropsWithoutRef<"svg">) {
 }
 
 export const socialMediaProfiles = [
-  { title: "Facebook", href: "https://facebook.com", icon: FacebookIcon },
-  { title: "Instagram", href: "https://instagram.com", icon: InstagramIcon },
+  { title: "Facebook", href: "https://www.facebook.com/profile.php?id=61563574500727", icon: FacebookIcon },
+  { title: "Instagram", href: "https://www.instagram.com/doublewordlabs/", icon: InstagramIcon },
   { title: "GitHub", href: "https://github.com", icon: GitHubIcon },
   { title: "Dribbble", href: "https://dribbble.com", icon: DribbbleIcon },
 ];
